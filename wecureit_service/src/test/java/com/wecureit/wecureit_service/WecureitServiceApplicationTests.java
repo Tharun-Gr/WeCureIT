db9dfa46d8b5e19b30328ea02eebda88bf25e2f3
Tharun Ganapathi Raman <tharun.2811@gmail.com>
2023-03-24 13:10:48 -0400

    Service files setup

---
+++ wecureit_service/src/test/java/com/wecureit/wecureit_service/WecureitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecureit.wecureit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WecureitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
